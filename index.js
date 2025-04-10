d32c412cfeff3f104901b62cdf190b8b7c9ab4dc
DanielJames830 <60898919+DanielJames830@users.noreply.github.com>
2025-04-10 14:38:11 -0400

    fixed bug

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const http = require('http');
 const app = express();
 const cors = require('cors');
-const authenticateToken = require('./src/middleware/authMiddleware');
 
 app.use(express.json());
 app.use(cors());
@@ -12,7 +11,6 @@ app.use('/map', require('./src/routers/mapRouter'));
 app.use('/entity', require('./src/routers/entityRouter'));
 app.use('/user', require('./src/routers/userRouter'));
 
-const hostname = process.env.LOCAL_IP
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
